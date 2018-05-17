7c056665d59da6eacada6c5c922afb054948725e
DESKTOP-AMP8C2E\Chris <cjrobertson@outlook.com>
2018-05-16 18:11:52 -0700

    Initial project upload.

---
+++ Assets/MediaPlayer/Scripts/MediaPlayer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class MediaPlayer :  IDisposable
+{
+    public bool Repeat { get; set; }
+
+    // The most important DLL ever since this is what will allow us to send commands to play, stop, etc.
+    [DllImport("winmm.dll")]
+    private static extern long mciSendString(string strCommand,
+        StringBuilder strReturn, int iRetrunLength, IntPtr hwndCallback);
+
+    void Start()
+    {
+        Repeat = false;
+    }
+
+    /* Media Player Functions. */
+
+    public MediaPlayer(string fileName)
+    {
+        const string FORMAT = @"open ""{0}"" type mpegvideo alias MediaFile";
+        string command = String.Format(FORMAT, fileName);
+        mciSendString(command, null, 0, IntPtr.Zero);
+    }
+
+    public void Play()
+    {
+        string command = "play MediaFile";
+        if (Repeat) command += " REPEAT";
+        mciSendString(command, null, 0, IntPtr.Zero);
+    }
+
+    // TO-DO: Figure out why the below 'NOTE' happens. Has something to do with the winmm.dll.
+    // NOTE: The stop command exhibits the same behavior as the Pause command. Not quite sure why.
+    public void Stop()
+    {
+        string command = "stop MediaFile";
+        mciSendString(command, null, 0, IntPtr.Zero);
+    }
+
+    // Exists after implementing the IDispose interface.
+    // Closes the opened media file.
+    public void Dispose()
+    {
+        string command = "close MediaFile";
+        mciSendString(command, null, 0, IntPtr.Zero);
+    }
+
+    /* End Media Player Functions */
+}
---
+++ Assets/MediaPlayer/Scripts/UI.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    private MediaPlayer mp3Player;
+    public Button openButton, playButton, stopButton;
+    public Toggle repeatToggle;
+
+    [Tooltip("The hardcoded path to the sound file wanting to be played. NOTE: Will only play one song. Multi-song chooser support coming soon.")]
+    public string chosenMusicFile = "C:\\Users\\JohnSmith\\Music\\TestAudio.mp3";
+
+    void Start()
+    {
+        // Calls the respective 'OnClick' method when you click a Button.
+        openButton.onClick.AddListener(OpenOnClick);
+        playButton.onClick.AddListener(PlayOnClick);
+        stopButton.onClick.AddListener(StopOnClick);
+
+        // Calls the required delegate to toggle the repeatToggle on/off.
+        repeatToggle.onValueChanged.AddListener(delegate {ToggleValueChanged(repeatToggle);});
+    }
+
+    // Repeat Toggle Method
+    void ToggleValueChanged(Toggle change)
+    {
+        Debug.Log("Repeat Toggled:");
+
+        if (mp3Player != null)
+        {
+            Debug.Log("Repeat Toggled: " + repeatToggle.isOn);
+            mp3Player.Repeat = repeatToggle.isOn;
+        }
+    }
+
+    /* Button Methods */
+
+    void OpenOnClick()
+    {
+        Debug.Log("Open Button Clicked!");
+
+        if (mp3Player != null)
+        {
+            mp3Player.Dispose();
+        }
+
+        mp3Player = new MediaPlayer(chosenMusicFile);
+    }
+
+    void PlayOnClick()
+    {
+        Debug.Log("Play Button Clicked!");
+
+        if (mp3Player != null)
+        {
+            mp3Player.Play();
+        }
+    }
+
+    void StopOnClick()
+    {
+        Debug.Log("Stop Button Clicked!");
+
+        if (mp3Player != null)
+        {
+            mp3Player.Stop();
+
+            /* NOTE: The stop functionality for some reason pauses the music rather than stopping and resetting the play time to the beginning. To hack around this, after stopping we'll dispose of the media player 
+             * and then immediately recreate it. This hack only works because this example project currently only supports hardcoded paths to one specific song. */
+            mp3Player.Dispose();
+            mp3Player = new MediaPlayer(chosenMusicFile);
+        }
+    }
+
+    /* End Button Methods */
+}
